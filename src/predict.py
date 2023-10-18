752efac76ae5cf4e3ce2636b8ff34a77c4ac79e4
Junel Solis <junel.solis@abo.fi>
2023-10-18 20:32:14 +0300

    add prediction script

---
+++ src/predict.py
@@ -0,0 +1,48 @@
+import numpy as np
+import os
+from glob import glob
+from tqdm import tqdm
+from skimage import io, img_as_uint
+from stardist.models import StarDist2D
+import shutil
+from csbdeep.utils import normalize
+
+# initialize directory constants
+# DATA_DIR is source data
+DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
+OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "..", "output")
+
+if __name__ == "__main__":
+    shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+    os.makedirs(OUTPUT_DIR)
+
+    model = StarDist2D.from_pretrained("2D_versatile_fluo")
+
+    img_paths = sorted(glob(os.path.join(DATA_DIR, "*.tif")))
+
+    for p in img_paths:
+        img = io.imread(p)
+        
+        # check number of dimensions, and that frame dimension is the first
+        assert img.ndim == 3
+        assert img.shape[0] < img.shape[1]
+
+        # initiate a list
+        frame_labels = []
+
+        # iterate through frames along axis 0
+        print(f"Predicting frames for {os.path.basename(p)}")
+        for f in tqdm(range(img.shape[0])):
+            frame = img[f]
+            labels, _ = model.predict_instances(normalize(frame))
+
+            # append 16-bit uint to list
+            frame_labels.append(img_as_uint(labels))
+
+        frame_labels = np.array(frame_labels)
+
+        io.imsave(
+            os.path.join(OUTPUT_DIR, os.path.basename(p)),
+            frame_labels,
+            check_contrast=False,
+        )
